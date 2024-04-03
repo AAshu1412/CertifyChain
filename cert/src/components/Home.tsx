c95f7153f2bf956ace030d99eb372101c0742dfd
AAshu1412 <ashutoshsanjeevmittal@gmail.com>
2024-04-03 23:52:32 +0530

    work

--- cert/src/components/Home.tsx
+++ cert/src/components/Home.tsx
@@ -1,22 +1,45 @@
-import { Flex, Box, Avatar, HStack, Spacer, Button, Container,Text, Heading, VStack, SimpleGrid ,Image} from "@chakra-ui/react";
+import {
+  Flex,
+  Box,
+  Container,
+  Text,
+  Heading,
+  VStack,
+  Image,
+} from "@chakra-ui/react";
 import certificate from "../assets/home1.jpeg";
 
-
-export default function Home(){
-    return(
-        <Box>
-            <Container  maxWidth="8xl" mt="30px" >
-                <Flex gap="100px" alignItems="center" justifyContent="center">
-      <VStack alignItems="center" justifyContent="center" height="50vh"  gap="20px">
-""
-<Box maxWidth="4xl" textAlign="center" alignItems="center" justifyContent="center" borderStyle="solid" borderColor="black">
-            <Heading fontSize="100px">Certify Chain</Heading>
-            <Text fontSize="30px" >Decentralized Documents and Certification Minting and Verifying Platform</Text>
-        </Box>
-<Text>Address : 0x00000000000000000000</Text>
-        </VStack>
-        <Image src={certificate} width="500px" borderRadius="20px"></Image></Flex></Container>
-        </Box>
-  
-    )
+export default function Home() {
+  return (
+    <Box>
+      <Container maxWidth="8xl" mt="30px">
+        <Flex gap="100px" alignItems="center" justifyContent="center">
+          <VStack
+            alignItems="center"
+            justifyContent="center"
+            height="50vh"
+            gap="20px"
+          >
+            ""
+            <Box
+              maxWidth="4xl"
+              textAlign="center"
+              alignItems="center"
+              justifyContent="center"
+              borderStyle="solid"
+              borderColor="black"
+            >
+              <Heading fontSize="100px">Certify Chain</Heading>
+              <Text fontSize="30px">
+                Decentralized Documents and Certification Minting and Verifying
+                Platform
+              </Text>
+            </Box>
+            <Text>Address : 0x00000000000000000000</Text>
+          </VStack>
+          <Image src={certificate} width="500px" borderRadius="20px"></Image>
+        </Flex>
+      </Container>
+    </Box>
+  );
 }
